0e773a8a275b7a516e711ecf9a88b2ad7074b453
DESKTOP-01F6T2L\James <jdikitanan_ccs@uspf.edu.ph>
2024-05-14 10:48:54 +0800

    doulbled the highschool

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ const profile = {
     'Junior Highschool (JHS)',
     'Gr7-G10 (2015-2020)',
     'University of San Carlos North Campus (USC NC)',
+  ],
+
+  highschools: [
     'Junior Highschool (JHS)',
     'Gr7-G10 (2015-2020)',
     'University of San Carlos North Campus (USC NC)',
   ],
+
 
   skills: [
     'Average proficiency in front-end technologies: HTML, CSS, and JavaScript.',
