46afbf85a9ac0fa9cbd7073ef602b7046b87c522
DESKTOP-01F6T2L\James <jdikitanan_ccs@uspf.edu.ph>
2024-05-14 11:07:17 +0800

    re-arranged data

--- index.js
+++ index.js
@@ -1,51 +1,64 @@
-const express = require('express');
-const cors = require('cors'); 
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
 const profile = {
-  education: {
-    degree: 'Bachelor of Science in Information Technology (BSIT)',
-    year: '2nd Year Student (2023-2024)',
-    university: 'University of Southern Philippines Foundation (USPF)'
-  },
+  education: [
+    {
+      degree: "Bachelor of Science in Information Technology (BSIT)",
+      year: "2nd Year Student (2023-2024)",
+      university: "University of Southern Philippines Foundation (USPF)",
+    },
+  ],
 
   highschools: [
-    'Junior Highschool (JHS)',
-    'Gr7-G10 (2015-2020)',
-    'University of San Carlos North Campus (USC NC)',
+    {
+      year_level: "Junior Highschool (JHS)",
+      year_duration: "Gr7-G10 (2015-2020)",
+      school: "University of San Carlos North Campus (USC NC)",
+    },
   ],
 
   skills: [
-    'Average proficiency in front-end technologies: HTML, CSS, and JavaScript.',
-    'Strong graphic design skills.',
-    '10 years of experience in video editing.',
-    'Excellent communication and teamwork abilities.'
+    {
+      description: [
+        "Average proficiency in front-end technologies: HTML, CSS, and JavaScript.",
+        "Strong graphic design skills.",
+        "10 years of experience in video editing.",
+        "Excellent communication and teamwork abilities.",
+      ],
+    },
   ],
 
   experience: [
-    'Proficient video editor with a decade of experience.',
-    'Extensive skills in crafting compelling visual narratives across multiple platforms.',
-    'Dedication to excellence demonstrated through a side hustle in video editing.'
+    {
+      description: [
+        "Proficient video editor with a decade of experience.",
+        "Extensive skills in crafting compelling visual narratives across multiple platforms.",
+        "Dedication to excellence demonstrated through a side hustle in video editing.",
+      ],
+    },
   ],
   awards: [
-    'Dean\'s List, USPF',
-    '1st placer in intramurals softball'
+    {
+      description: ["Dean's List, USPF", "1st placer in intramurals softball"],
+    },
   ],
   personalInfo: {
-    name: 'James Stephen B Dikitanan',
-    address: 'Lahug, Cebu City',
-    phoneNumber: '09430518711',
-    email: 'jdikitanan_ccs@uspf.edu.ph',
-    birthdate: 'June 7 2003',
-    sex: 'Male'
-  }
+    name: "James Stephen B Dikitanan",
+    address: "Lahug, Cebu City",
+    phoneNumber: "09430518711",
+    email: "jdikitanan_ccs@uspf.edu.ph",
+    birthdate: "June 7 2003",
+    sex: "Male",
+  },
 };
 
 // Enable CORS for all routes
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json(profile);
 });
 
